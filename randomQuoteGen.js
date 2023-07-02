3e8d4adc85868bfeadb99eb596639fe6abff3561
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-07-01 19:36:56 -0500

    Update randomQuoteGen.js
    created an array of objects with elements for Quotes and Authors.

--- randomQuoteGen.js
+++ randomQuoteGen.js
@@ -1,1 +1,19 @@
 //random quote generator JS code
+quoteObjectsArray = [
+  {quote: "We out Here!",
+   author: "Mike W."},
+  {quote: "Drop it low!",
+   author: "Lil John"},
+  {quote: "No one exists like that, these are people who do not want others to feel the same pain that they have, and sacrified everything to do so",
+  author: "Tanjiro, Demon Slayer"},
+  {quote: "I believe in you, and need you to understand you have the power to change",
+  author: "Nick H."},
+  {quote: "We’re outsiders, so wouldn’t that be ‘muddling’?",
+  author: "Goofy, KH2"},
+  {quote: "I'm sick of following my dreams, man. I'm just going to ask where they're going and hook up with'em later.",
+  author: "Mitch Hedberg"},
+  {quote: "Do not take Life too seriously. You'll never get out of it alive.",
+  author: "Elbert Hubbard"},
+  {quote: "I'd like to live like a poor man - only with a lot of money.",
+  author: "Pablo Picasso"},
+  ];
